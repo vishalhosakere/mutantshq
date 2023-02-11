be4b08e26292329283933fbe4e79e7d827de3294
Vishal Hosakere <mailme.vishalh@gmail.com>
2023-02-11 02:37:06 -0800

    Fix the Switch color to match overall color scheme

--- src/components/FilterRowSwitch.tsx
+++ src/components/FilterRowSwitch.tsx
@@ -19,7 +19,7 @@ export default function FilterRowSwitch({
         <Switch
           checked={state}
           onChange={setState}
-          className={`${state ? "bg-accent-light" : "bg-accent-dark"}
+          className={`${state ? "bg-accent" : "bg-accent-darker"}
           relative inline-flex h-7 w-14 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-accent`}
         >
           <span className="sr-only">{`Set ${label} filter`}</span>
