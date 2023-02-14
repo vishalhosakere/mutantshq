fe71a4b51be48509807a81635a341d8afb94fb5c
Vishal Hosakere <mailme.vishalh@gmail.com>
2023-02-13 21:33:56 -0800

    * Remove Logo * Show Mutants HQ instead of Logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,6 @@
 import { Disclosure } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import NavButton from "./NavButton";
-import Logo from "./Logo";
-import Link from "next/link";
 import NavAsideButton from "./NavAsideButton";
 import { NavLinkItems } from "@/utils/NavLinkItems";
 
@@ -15,11 +13,11 @@ export default function NavBar() {
     >
       {({ open }) => (
         <>
-          <div className="mx-auto px-2 md:px-6 lg:px-8">
+          <div className="mx-auto px-2 md:px-10">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-whitish hover:bg-gray-700 focus-accent">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-whitish hover:bg-grayish focus-accent">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
@@ -29,22 +27,12 @@ export default function NavBar() {
                 </Disclosure.Button>
               </div>
 
-              <div className="absolute inset-y-0 left-10 items-center hidden md:flex">
-                <Link
-                  href="/"
-                  className="fill-gray-300 hover:fill-white hover:scale-110 flex h-full items-center"
-                >
-                  <Logo />
-                </Link>
+              <div className="absolute inset-y-0 items-center hidden md:flex">
+                <h1 className="text-xl font-bold text-whitish">MUTANTS HQ</h1>
               </div>
-              <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-center">
-                <div className="flex flex-shrink-0 items-center">
-                  <Link
-                    href="/"
-                    className="fill-gray-300 hover:fill-white hover:scale-110 md:hidden"
-                  >
-                    <Logo />
-                  </Link>
+              <div className="flex flex-1 items-center justify-center md:justify-end lg:justify-center">
+                <div className="flex flex-shrink-0 items-center md:hidden">
+                  <h1 className="text-xl font-bold text-whitish">MUTANTS HQ</h1>{" "}
                 </div>
                 <div className="hidden md:ml-6 md:block">
                   <div className="flex space-x-4 pt-0.5">
